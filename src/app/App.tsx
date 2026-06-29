8e5f49cb531472229323d1c35de83dd8cb3e1d91
omadbek-mark <omadbekanvarbekov00@gmail.com>
2026-06-29 22:31:24 +0900

    feat: publish otherNavbar header component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
 import { HomeNavbar } from "./components/headers/HomeNavbar";
-import { OtherNavbar } from "./components/headers/OtherNavbar";
+import  OtherNavbar  from "./components/headers/OtherNavbar";
 import { Footer } from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
