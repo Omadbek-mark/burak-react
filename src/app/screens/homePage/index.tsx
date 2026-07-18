ea3bc68e30492d9035e228e416629e5db23ad009
omadbek-mark <omadbekanvarbekov00@gmail.com>
2026-07-18 15:40:43 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,14 +7,28 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrieveNewDishes, retrievePopularDishes } from "./selector";
+import { Product } from "../../../lib/types/product";
+
+/** REDUX SLICE & SELECTOR **/
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)), // bu setPopularDishes => reducer
+});
+
+const popularDishesRetriever = createSelector(
+retrievePopularDishes, (popularDishes) => ({ popularDishes })
+);
+
 export default function HomePage() {
-  // Selector: Store => Data
+  const { setPopularDishes } = actionDispatch(useDispatch());
+  /** Bu setPopularDishes esa bizning command **/
+  const { popularDishes } = useSelector(popularDishesRetriever); 
 
-  useEffect(() => {
-    // Backend server data request => DATA
-
-    // Slice: Data => Store
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className={"homepage"}>
@@ -27,3 +41,9 @@ export default function HomePage() {
     </div>
   );
 }
+
+
+/** 
+  yuqorida biz kiritgan malumotlar yani backend dan kelishi kerak bolgan malumotlar reducer ga dispatch lar orqali kirib kelyapti, va biz dispatch lar orqali setPopularDishes reducerga malumot ni payload qilyabmiz, va biz yuklagan malumot actionni payload qismidan qabul qilinyapti va state dagi setPopularDishes ni qiymatini ozgartiryapti.
+  Selector esa bizga eng oxirgi ozgargan qiymatni olib beryapti
+**/
